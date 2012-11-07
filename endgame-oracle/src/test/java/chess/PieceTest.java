1c310f8207c5d7819fed3c466f9430f6a996df43
Kestutis-Z <k292929@gmail.com>
2012-11-07 22:54:54 +0000

    Passed testAllAbbreviationsOfPieces

--- endgame-oracle/src/test/java/chess/PieceTest.java
+++ endgame-oracle/src/test/java/chess/PieceTest.java
@@ -2,6 +2,9 @@ package chess;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PieceTest {
@@ -29,7 +32,37 @@ public class PieceTest {
 
     @Test
     public void testAllAbbreviationsOfPieces() {
+	List<String> expectedList = new ArrayList<>();
 	
+	expectedList.add("WK");
+	expectedList.add("WQ");
+	expectedList.add("WQ2");
+	expectedList.add("WR");
+	expectedList.add("WR2");
+	expectedList.add("WB");
+	expectedList.add("WB2");
+	expectedList.add("WN");
+	expectedList.add("WN2");
+	expectedList.add("WP");
+	expectedList.add("WP2");
+	expectedList.add("WP3");
+	
+	expectedList.add("BK");
+	expectedList.add("BQ");
+	expectedList.add("BQ2");
+	expectedList.add("BR");
+	expectedList.add("BR2");
+	expectedList.add("BB");
+	expectedList.add("BB2");
+	expectedList.add("BN");
+	expectedList.add("BN2");
+	expectedList.add("BP");
+	expectedList.add("BP2");
+	expectedList.add("BP3");
+	
+	List<String> actualList = Piece.allAbbreviationsOfPieces();
+	
+	assertEquals(expectedList, actualList);
     }
     
 }
