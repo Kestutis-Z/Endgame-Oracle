ed0f97c527cb0b8f471cd2f3dd8d5d1acbf1083c
Kestutis-Z <k292929@gmail.com>
2012-11-24 19:05:56 +0000

    Modify javadoc

--- endgame-oracle/src/main/java/tablebases/EndgameTablebasesProbingCodeAPI.java
+++ endgame-oracle/src/main/java/tablebases/EndgameTablebasesProbingCodeAPI.java
@@ -13,15 +13,27 @@ import chess.ChessPositionEvaluation.ChessPositionEvaluationWithDTM;
  */
 public interface EndgameTablebasesProbingCodeAPI {
     
-    /** Probes tablebase for the specified chess position. 
-     * @return basic evaluation (one of "White wins" / 
-     * "Black wins" / "Draw" / "Illegal position") only. */
+    /**
+     * Probes tablebase for the basic evaluation of the specified 
+     * chess position.
+     *
+     * @param chessPosition representation of the chess position: side to move
+     * (White / Black), White and Black pieces, and their respective squares
+     * @return game-theoretical value of the chess position obtained
+     * from the tablebase
+     */
     public abstract ChessPositionEvaluation 
     	queryTablebaseForResultOnly(ChessPosition chessPosition);
     
-    /** Probes tablebase for the specified chess position. 
+    /**
+     * Probes tablebase for the advanced evaluation of the specified 
+     * chess position. 
+     *
+     * @param chessPosition representation of the chess position: side to move
+     * (White / Black), White and Black pieces, and their respective squares
      * @return evaluation and distance to mate (a non-negative 
-     * number in case the evaluation is "White wins" / "Black wins"). */
+     * number in case the evaluation is "White wins" / "Black wins")
+     */
     public abstract ChessPositionEvaluationWithDTM 
     	queryTablebaseForResultAndDTM(ChessPosition chessPosition);    
 
