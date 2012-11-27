f48fc94906d482cf567034f259d21403bfe90ae3
Kestutis-Z <k292929@gmail.com>
2012-11-27 21:51:28 +0000

    New interface ChessEntity

---
+++ endgame-oracle/src/main/java/chess/patterns/ChessEntity.java
@@ -0,0 +1,10 @@
+package chess.patterns;
+
+/**
+ * Tagging interface - a common parent interface to be extended by child
+ * interfaces representing various entities of chess.
+ * 
+ * @author Kestutis
+ * 
+ */
+public interface ChessEntity { }
